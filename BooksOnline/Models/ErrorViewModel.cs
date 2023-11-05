150a2904971f763e28efd95ea3fb15140dff909c
Iamneo <techadmin@iamneo.ai>
2023-11-05 12:38:41 +0000

    Backup Commit

--- BooksOnline/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BooksOnline.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ dotnetapp1/Models/Category.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnetapp1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id{get;set;}
+
+        [Required]
+        public string Name{get;set;}
+
+        public string DisplayOrder{get;set;}
+
+    }
+}
