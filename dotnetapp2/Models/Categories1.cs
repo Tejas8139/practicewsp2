e8513014432416d3ff1c9d85549a0528066ba4c4
Iamneo <techadmin@iamneo.ai>
2023-11-07 16:48:53 +0000

    Backup Commit

--- dotnetapp2/Models/Categories1.cs
+++ dotnetapp2/Models/Categories1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dotnetapp2.Models
 {
@@ -12,8 +14,10 @@ namespace dotnetapp2.Models
         public int Id{get;set;}
 
         [Required]
+        [DisplayName("Category Name")]
         public string Name {get;set;} 
 
+        [DisplayName("Display Order")]
         public int DisplayOrder{get;set;}
 
     }
