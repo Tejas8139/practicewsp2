e798260502350b81748eb5aa1f7ebf0bb6b9d2b1
Iamneo <techadmin@iamneo.ai>
2023-11-14 15:51:17 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tejas';
-  
+  imgUrL='https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
 }
