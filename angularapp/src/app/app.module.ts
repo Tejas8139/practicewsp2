2f4531621aba2b266dae7a49c9e22cc065f70099
Iamneo <techadmin@iamneo.ai>
2023-11-14 11:51:17 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,17 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoListComponent
+    AppComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    TodoListComponent
+    AppRoutingModule
+    
   
 
     
