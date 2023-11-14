3ce7a8b8c257ef2b68f4a7f6b2cae3f46b355547
Iamneo <techadmin@iamneo.ai>
2023-11-14 17:51:14 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmpComponent } from './emp/emp.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'emp',component:EmpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ angularapp/src/app/emp/emp.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+@Component({
+  selector: 'app-emp',
+  templateUrl: './emp.component.html',
+  styleUrls: ['./emp.component.css']
+})
+export class EmpComponent implements OnInit {
+
+  constructor(private formbuilder:FormBuilder) { }
+empform=this.formbuilder.group({
+  name:['',[Validators.required,Validators.maxLength(40)]],
+  email:['',Validators.email],
+  city:['',Validators.pattern("[a-zA-Z]")]
+})
+save():void{
+  console.log(this.empform.value)
+}
+  ngOnInit() {
+  }
+
+}
