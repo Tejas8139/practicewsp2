d40ebb365a50b17ddf1c8e414e4d44d55d48a718
Iamneo <techadmin@iamneo.ai>
2023-11-14 15:21:17 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: `<p> Hello </p>`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
