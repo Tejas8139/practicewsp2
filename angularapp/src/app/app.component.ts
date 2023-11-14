73fed5826709784e029353dbfc77ee03b770dd7d
Iamneo <techadmin@iamneo.ai>
2023-11-14 15:36:17 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -5,8 +5,10 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `<p> Hello </p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'Tejas';
+  
 }
